6ca0a822cbce44baf743cb2cb7f11ee3de463347
Artem_PC <taftoos2@gmail.com>
2025-04-08 22:33:15 +0300

    feat: solve getIdGeneratorFunction test

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -206,8 +206,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let counts = startFrom;
+  function counter() {
+    counts += 1;
+    return counts - 1;
+  }
+  return counter;
 }
 
 module.exports = {
