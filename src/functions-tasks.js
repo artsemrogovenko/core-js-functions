5597792b723ea20da99947b5813806d3ab044271
Artem_PC <taftoos2@gmail.com>
2025-04-08 20:32:56 +0300

    feat: solve getArgumentsCount test

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -50,8 +50,12 @@ function getFunctionBody(func) {
  *  ]) => [0, 1, 2]
  *
  */
-function getArgumentsCount(/* funcs */) {
-  throw new Error('Not implemented');
+function getArgumentsCount(funcs) {
+  const array = [];
+  [...funcs].forEach((func) => {
+    array.push(func.length);
+  });
+  return array;
 }
 
 /**
